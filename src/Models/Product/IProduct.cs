2a13184849905a462c6b4bd60676c945a9c827a6
Eduardo Silva <eduardo.lour.silva@gmail.com>
2019-07-11 12:28:40 -0300

    added comments about methods must be properties

--- src/Models/Product/IProduct.cs
+++ src/Models/Product/IProduct.cs
@@ -3,6 +3,7 @@ namespace Exemplo.Models.Products
     public interface IProduct
     {
         int Id { get; }
+        // Melhor transformar esses métodos em propriedades, vamos pensar em outras formas para para fazer overload
         decimal GetUnitPrice();
         string GetDescription();
     }
