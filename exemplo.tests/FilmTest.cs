7a100100f1efdc3e63ab7b4b1598c8551d7195eb
Gabriel G <gabriel.lima@itlab.com.br>
2019-09-12 23:32:30 -0300

    Override example

--- exemplo.tests/FilmTest.cs
+++ exemplo.tests/FilmTest.cs
@@ -8,26 +8,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace exemplo.tests
 {
     [TestClass]
-    public class FilmTest
+    public class FilmTest : TestBase
     {
+        private Application applicationInstance;
+        
+        public FilmTest() : base()
+        {
+            Setup();
+        }
+
+        public override void Setup()
+        {
+            applicationInstance = new Application();
+        }
+
         [TestMethod]
         public void AddFilm_ReturnsAddedFilm()
         {
-            var instance = new Application();
             var titanic = new Film()
             {
                 Title = "Titanic",
-                Genres = new List<Genre>(){Genre.Drama},
+                Genres = new List<Genre>() { Genre.Drama },
                 Film3D = Option3D.With3D,
                 MinimumAge = 8,
-                ReleaseDate = new System.DateTime(1998,1,16),
-                RunningTime = new System.TimeSpan(3,15,0)
+                ReleaseDate = new System.DateTime(1998, 1, 16),
+                RunningTime = new System.TimeSpan(3, 15, 0)
             };
-            
-            instance.AddFilm(titanic);
 
-            Assert.IsTrue(instance.Films.Count() == 1);
-            Assert.AreEqual(instance.Films.FirstOrDefault(), titanic);
+            applicationInstance.AddFilm(titanic);
+
+            Assert.IsTrue(applicationInstance.Films.Count() == 1);
+            Assert.AreEqual(applicationInstance.Films.FirstOrDefault(), titanic);
         }
     }
 }
